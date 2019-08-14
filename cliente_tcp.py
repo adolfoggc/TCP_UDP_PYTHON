c8b88351876a93ba4e4613209f8d94fe9392ceb5
Carlos Adolfo <adolfo.ggc@gmail.com>
2019-08-13 21:18:29 -0300

    recebendo dados

--- cliente_tcp.py
+++ cliente_tcp.py
@@ -1,12 +1,22 @@
 import socket
-HOST = '127.0.0.1'     # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-print 'Para sair use CTRL+X\n'
-msg = raw_input()
-while msg <> '\x18':
-    tcp.send (msg)
-    msg = raw_input()
-tcp.close()
+
+def turn_on(HOST, PORT):
+	HOST = '127.0.0.1'     # Endereco IP do Servidor
+	PORT = 5000            # Porta que o Servidor esta
+	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	dest = (HOST, PORT)
+	tcp.connect(dest)
+	print 'Para sair use \CLOSE\n'
+	msg = tcp.recv(1024)
+	print(msg)
+	msg = raw_input()
+	while msg <> '\CLOSE':
+	    tcp.send (msg)
+	    msg = raw_input()
+
+	tcp.close()
+
+
+h = raw_input("Digite o host que deseja alcancar: ")
+p = int(raw_input("Digite a porta a qual deseja enviar: "))
+turn_on(h,p)
--- servidor_tcp.py
+++ servidor_tcp.py
@@ -1,17 +1,34 @@
 import socket
+import datetime
 
-HOST = ''              # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-tcp.bind(orig)
-tcp.listen(1)
-while True:
-    con, cliente = tcp.accept()
-    print 'Concetado por', cliente
-    while True:
-        msg = con.recv(1024)
-        if not msg: break
-        print cliente, msg
-    print 'Finalizando conexao do cliente', cliente
-    con.close()
+
+def turn_on(PORT):
+	HOST = ''              # Endereco IP do Servidor
+	#PORT = 5000            # Porta que o Servidor esta
+	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	orig = (HOST, PORT)
+	tcp.bind(orig)
+	tcp.listen(1)
+	print 'Iniciando servidor na porta', PORT
+	reqnum = 0
+	starting_time = datetime.datetime.now()
+    #starting_time = datetime.strptime(starting_time, '%H:%M:%S')
+	while True:
+	    con, cliente = tcp.accept()
+	    print 'Concetado por', cliente
+	    con.send('Bem-vindo, meu citoplasma!')
+	    while True:
+	        msg = con.recv(1024)
+	        if not msg: break
+	        if(msg == '\REQNUM'):
+	        	reqnum += 1
+	        	print(str(reqnum))
+	        elif(msg == '\UPTIME'):
+	        	print(str(datetime.datetime.now() - starting_time))
+	        else:
+	        	print cliente, msg
+	    print 'Finalizando conexao do cliente', cliente
+	    con.close()
+
+#p = int(input("Digite a porta que deseja ouvir: "))
+turn_on(5000)
